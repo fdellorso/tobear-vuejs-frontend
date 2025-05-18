2e266ce4d985d36cd38e9f19fd2d57a84227e082
francesco.dellorso@gmail.com <francesco.dellorso@gmail.com>
2025-05-18 08:56:36 +0200

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
