fbe7104a122f99cfd27d828dcf51fbde8682b0d1
francesco.dellorso@gmail.com <francesco.dellorso@gmail.com>
2025-05-18 10:11:13 +0200

    axios try

--- src/axios/index.js
+++ src/axios/index.js
@@ -8,7 +8,7 @@ const axiosClient = axios.create({
 })
 
 const axiosCSRF = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL.replace('/api', ''), // es. https://tobearapi.rf.gd
+  baseURL: '', // es. https://tobearapi.rf.gd
   withCredentials: true, // Include credentials in the request headers
   withXSRFToken: true, // Include XSRF-TOKEN header in the request headers
 })
