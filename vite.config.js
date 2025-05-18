af81086ca7e4d516aa26715735f65658e43ff0b5
francesco.dellorso@gmail.com <francesco.dellorso@gmail.com>
2025-05-18 09:05:53 +0200

    test without pwa

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
