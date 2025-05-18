fd3073f9ea893a8c65dd739fcc80313e898a64a6
francesco.dellorso@gmail.com <francesco.dellorso@gmail.com>
2025-05-18 09:49:46 +0200

    add loadenv to vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import process from 'process'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -8,17 +9,22 @@ import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.VITE_API_BASE_URL,
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    mode,
+    base: env.VITE_BASE_URL || '/',
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: ['laravel.fritz.box'],
-  },
-})
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      allowedHosts: ['laravel.fritz.box'],
+    },
+  })
+}
