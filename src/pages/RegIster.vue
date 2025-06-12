140c25091bfcde3434acddb231b3fa13eb532a59
francesco.dellorso@gmail.com <francesco.dellorso@gmail.com>
2025-06-12 07:43:05 +0200

    auto generate paswword from browser

--- src/pages/RegIster.vue
+++ src/pages/RegIster.vue
@@ -87,6 +87,7 @@ function submit() {
             type="password"
             name="password"
             id="password"
+            autocomplete="new-password"
             v-model="data.password"
             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
           />
@@ -107,6 +108,7 @@ function submit() {
             type="password"
             name="password"
             id="passwordConfirmation"
+            autocomplete="new-password"
             v-model="data.password_confirmation"
             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
           />
